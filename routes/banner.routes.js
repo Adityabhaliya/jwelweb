4089af7290fe578149f8b6e73fa987bc1c769866
Adityabhaliya <adityabhaliyamaruti@gmail.com>
2025-08-02 00:30:31 +0530

    DD

--- routes/banner.routes.js
+++ routes/banner.routes.js
@@ -6,7 +6,7 @@ const { verifyAdminToken } = require('../config/token');
 router.post('/admin/banner',verifyAdminToken, bannerController.createBanner);
 router.get('/admin/banner',verifyAdminToken, bannerController.getAllBanners);
 router.get('/admin/banner/:id',verifyAdminToken, bannerController.getBannerById);
-router.put('admin/banner/:id',verifyAdminToken, bannerController.updateBanner);
-router.delete('admin/banner/:id',verifyAdminToken, bannerController.deleteBanner);
+router.put('/admin/banner/:id',verifyAdminToken, bannerController.updateBanner);
+router.delete('/admin/banner/:id',verifyAdminToken, bannerController.deleteBanner);
 
 module.exports = router;
