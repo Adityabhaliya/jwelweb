f41a8125e9a7fa0a92c675783bc6f627221609d4
Adityabhaliya <adityabhaliyamaruti@gmail.com>
2025-08-10 15:12:52 +0530

    dd

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -12,7 +12,8 @@ router.delete('/admin/product/:slug',verifyAdminToken, productController.deleteP
 router.get('/user/product', productController.getAllProductsUser); 
 router.get('/user/globel-product', productController.getAllProductsUserGlobel);
 router.get('/user/category-product', productController.getAllProductsUserCatSlug);
-router.get('/user/recommand-product', productController.getAllProductsUserRecommand);
+router.get('/user/recommand-product', productController.getAllProductsUserRecommand); 
+router.get('/user/new-product', productController.getAllProductsUserNew);
 router.get('/user/product-details/:slug',verifyAdminToken, productController.getProductBySlugUser);
 
 
